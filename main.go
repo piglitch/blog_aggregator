45fca180b48a22a4bda91f07b6957b33c4c16958
Avi Banerjee <avibanerjee2000@gmail.com>
2025-02-21 15:59:49 +0000

    run and register work

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 	"path/filepath"
 )
 
-
 type Config struct {
-	DbUrl string `json:"db_url"`
+	DbUrl       string `json:"db_url"`
 	CurrentUser string `json:"current_user_name"`
 }
 
@@ -27,13 +26,19 @@ type commands struct {
 	cmdName map[string]func(*state, command, string) error
 }
 
-func getConfigFilePath(cfgpath string) (string, error){
-	homeDir := "/mnt/f/blog_aggregator/"
+func getConfigFilePath(cfgpath string) (string, error) {
+	fmt.Println("getConfigFilePath: cfgpath =", cfgpath) // Added fmt.Println
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("cannot find home directory")
+		return "", err
+	}
 	configPath := filepath.Join(homeDir, cfgpath)
 	return configPath, nil
 }
 
 func Read(cfgpath string) (Config, error) {
+	fmt.Println("Read: cfgpath =", cfgpath) // Added fmt.Println
 	configPath, err := getConfigFilePath(cfgpath)
 	if err != nil {
 		return Config{}, err
@@ -50,8 +55,9 @@ func Read(cfgpath string) (Config, error) {
 }
 
 func (cfg *Config) SetUser(userName string, cfgPath string) error {
+	fmt.Println("SetUser: cfgPath =", cfgPath) // Added fmt.Println
 	filePath, err := getConfigFilePath(cfgPath)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	cfg.CurrentUser = userName
@@ -59,6 +65,7 @@ func (cfg *Config) SetUser(userName string, cfgPath string) error {
 	if err != nil {
 		return err
 	}
+	fmt.Println(filePath, "//// 68 ////")
 	if err := os.WriteFile(filePath, newData, 0644); err != nil {
 		return err
 	}
@@ -66,10 +73,12 @@ func (cfg *Config) SetUser(userName string, cfgPath string) error {
 }
 
 func handlerLogin(s *state, cmd command, cfgPath string) error {
+	fmt.Println("handlerLogin: cfgPath =", cfgPath) // Added fmt.Println
 	if len(cmd.args) == 0 {
-		return errors.New("No arguments passed in args")
+		os.Exit(1)
+		return errors.New("no arguments passed in args")
 	}
-	err := s.cfg.SetUser(cmd.name, cfgPath)
+	err := s.cfg.SetUser(cmd.args[0], cfgPath)
 	if err != nil {
 		return err
 	}
@@ -77,28 +86,50 @@ func handlerLogin(s *state, cmd command, cfgPath string) error {
 	return nil
 }
 
-func (c *commands) register(name string, f func(*state, command) error) {
-
+func (c *commands) register(name string, f func(*state, command, string) error) {
+	cmdMap := c.cmdName
+	cmdMap[name] = f
 }
 
 func (c *commands) run(s *state, cmd command) error {
-
+	const configFileName = ".gatorconfig.json"
+	err := handlerLogin(s, cmd, configFileName)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
 
-
-
-func main(){
-	const configFileName = "gatorconfig.json"
+func main() {
+	const configFileName = ".gatorconfig.json"
 	readCfg, err := Read(configFileName)
 	if err != nil {
-		return 
+		return
 	}
 	newState := state{
 		cfg: &readCfg,
 	}
-	cmdMap := make(map[string]func(s *state, cmd command, cfgPath string) error )
-	cmdMap["login"] = handlerLogin
+	cmdMap := make(
+		map[string]func(s *state, cmd command, cfgPath string) error,
+	)
 	newCommands := commands{
 		cmdName: cmdMap,
+	}
+	newCommands.register("login", handlerLogin)
+	cliArgs := os.Args
+	if len(cliArgs) < 2 {
+		fmt.Println("insufficient args")
+		os.Exit(1)
+		return
+	}
+	newCliCmd := command{
+		name: cliArgs[1],
+		args: cliArgs[2:],
+	}
+	fmt.Println(newCliCmd.name, newCliCmd.args)
+	err = newCommands.run(&newState, newCliCmd)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
